ece747de0fc7fbd8681c830fd4d2343bcab9fc90
Betina Cortés <b.cortes@uniandes.edu.co>
2023-03-09 21:26:15 -0500

    Update Script1.R

--- scripts/Script1.R
+++ scripts/Script1.R
@@ -9,15 +9,24 @@ rm(list = ls())
 
 # install.packages("pacman")
 library("pacman")
-p_load("tidyverse", "sf", "naniar", "tidymodels", "readxl", "psych","ranger","glmnet")
+p_load("tidyverse", "sf", "naniar", "tidymodels", "readxl", "psych","ranger","glmnet","naniar")
 
 setwd("/Users/betinacortes/Desktop/Repositorio_taller3")
 
 # Importing Dataset ----
-# Removing City and operation type as they dont add information 
-train <- read_csv("/Users/betinacortes/Desktop/Repositorio_taller3/stores/train.csv",
+# Removing City and operation type as they don't add information 
+train <- read_csv("stores/train.csv",
                   col_types = cols("price" = col_number())) |> 
   select(-c(city, operation_type))
-test <- read_csv("/Users/betinacortes/Desktop/Repositorio_taller3/stores/test.csv",
+test <- read_csv("stores/test.csv",
                  col_types = cols("price" = col_number())) |> 
   select(-c(city, operation_type))
+
+## Checking missing values ---- 
+miss_var_summary(train)
+miss_var_summary(test)
+
+# Given the high percentages of missing values in the columns surface_total,
+# surface covered, rooms and bathrooms in both the train and test data sets,
+# we wont use them as predictors. Instead, we will rely strongly  on the 
+# augmented variables.
