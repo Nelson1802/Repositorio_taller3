843ed71175debb3263b7323b8227c5af8042fc14
Betina Cortés <b.cortes@uniandes.edu.co>
2023-03-09 21:16:33 -0500

    Update Script1.R

--- scripts/Script1.R
+++ scripts/Script1.R
@@ -11,11 +11,13 @@ rm(list = ls())
 library("pacman")
 p_load("tidyverse", "sf", "naniar", "tidymodels", "readxl", "psych","ranger","glmnet")
 
+setwd("/Users/betinacortes/Desktop/Repositorio_taller3")
+
 # Importing Dataset ----
 # Removing City and operation type as they dont add information 
-train <- read_csv("Data/Kaggle/train.csv",
+train <- read_csv("/Users/betinacortes/Desktop/Repositorio_taller3/stores/train.csv",
                   col_types = cols("price" = col_number())) |> 
   select(-c(city, operation_type))
-test <- read_csv("Data/Kaggle/test.csv",
+test <- read_csv("/Users/betinacortes/Desktop/Repositorio_taller3/stores/test.csv",
                  col_types = cols("price" = col_number())) |> 
   select(-c(city, operation_type))
