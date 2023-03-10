e73e9d6e4ad7032247ac988652900e8de5a22c37
Betina Cortés <b.cortes@uniandes.edu.co>
2023-03-09 21:03:05 -0500

    Create Script1.R

---
+++ scripts/Script1.R
@@ -0,0 +1,12 @@
+################################################################
+# Problem Set 3: Script
+# Authors: Yilmer Palacios, Betina Cortés, Lida Jimena Cárdenas,
+# Nelson Fabián López
+################################################################
+
+# Loading Libraries ----
+rm(list = ls()) 
+
+# install.packages("pacman")
+library("pacman")
+p_load("tidyverse", "sf", "naniar", "tidymodels", "readxl", "psych","ranger","glmnet")
