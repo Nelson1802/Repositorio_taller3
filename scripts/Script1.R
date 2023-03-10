c5258560c19f6a21f74d88791d392a841c1aa3b4
Betina Cortés <b.cortes@uniandes.edu.co>
2023-03-09 22:21:54 -0500

    Update Script1.R

--- scripts/Script1.R
+++ scripts/Script1.R
@@ -57,4 +57,84 @@ pop_crimen <- read_xlsx("stores/Crimen/pop_crimen_upz.xlsx") |>
     year = as.numeric(year)
   ) 
 
+## Mapa de UPZ
+## Fuente: Catastro Distrital
+
+upz <- sf::st_read("stores/Geometries/UPZ/Upz.shp") |> 
+  st_transform(4326) |> 
+  mutate(CODIGO_UPZ = str_pad(CODIGO_UPZ, 3, pad = "0", side = "left"))
+
+## Cargar las bases de datos train y test como geometry con el paquete sf
+
+train_geo <- st_as_sf(train, coords = c("lon", "lat"), crs = 4326, remove = FALSE)
+test_geo <- st_as_sf(test, coords = c("lon", "lat"), crs = 4326, remove = FALSE)
+
+## UPZ para cada residencia
+
+train_upz <- st_join(train_geo, upz, join = st_within)
+test_upz <- st_join(test_geo, upz, join = st_within)
+
+## Unir tasa de homicidios y robo a residencias por UPZ
+
+train_crime <- train_upz |> 
+  left_join(pop_crimen, by = c("CODIGO_UPZ" = "Código UPZ",
+                               "year" = "year")) |> 
+  select(-c(OBJECTID, ZONA_ESTAC, DECRETO_PO, ACTO_ADMIN, AREA_HECTA,
+            SHAPE_Leng, SHAPE_Area, pop, hom, hurtor, `Nombre UPZ`))
+
+test_crime <- test_upz |> 
+  left_join(pop_crimen, by = c("CODIGO_UPZ" = "Código UPZ",
+                               "year" = "year")) |> 
+  select(-c(OBJECTID, ZONA_ESTAC, DECRETO_PO, ACTO_ADMIN, AREA_HECTA,
+            SHAPE_Leng, SHAPE_Area, pop, hom, hurtor, `Nombre UPZ`))
+
+## b. Información sobre servicios/amenidades cercanas: número de colegios, ips (hospitales) y parques en 250m
+## Fuente: IDECA - Catastro distrital
+
+## Colegios
+schools <- sf::st_read("stores/Geometries/Colegios/Colegios_2022_03.shp")
+
+## Hospitales
+ips <- sf::st_read("stores/Geometries/ips/ips.shp")
+
+## Parques
+parks <- sf::st_read("stores/Geometries/parques/parques.shp")
+
+## Contar colegios, parques y hospitales dentro de cada UPZ
+
+## Colegios en UPZ
+schools_upz <- st_join(schools, upz |> st_transform(st_crs(schools)),
+                       join = st_within) |> 
+  st_drop_geometry() |> 
+  count(CODIGO_UPZ, name = "n_schools") |> 
+  drop_na()
+
+## Parques en UPZ
+parks_upz <- st_join(parks, upz |> st_transform(st_crs(parks)),
+                     join = st_within) |> 
+  st_drop_geometry() |> 
+  count(CODIGO_UPZ, name = "n_parks") |> 
+  drop_na()
+
+## Hospitales in UPZ
+ips_upz <- st_join(ips, upz |> st_transform(st_crs(ips)),
+                   join = st_within) |> 
+  st_drop_geometry() |> 
+  count(CODIGO_UPZ, name = "n_ips") |> 
+  drop_na()
+
+## Agregar la información sobre servicios/amenidades cercanas a la base de datos anterior
+
+train_amenities <- train_crime |> 
+  left_join(schools_upz, by = "CODIGO_UPZ") |> 
+  left_join(parks_upz, by = "CODIGO_UPZ") |> 
+  left_join(ips_upz, by = "CODIGO_UPZ") |> 
+  replace_na(list(n_schools = 0, n_parks = 0, n_ips = 0))
+
+test_amenities <- test_crime |> 
+  left_join(schools_upz, by = "CODIGO_UPZ") |> 
+  left_join(parks_upz, by = "CODIGO_UPZ") |> 
+  left_join(ips_upz, by = "CODIGO_UPZ") |> 
+  replace_na(list(n_schools = 0, n_parks = 0, n_ips = 0))
+
 
